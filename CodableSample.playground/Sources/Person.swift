f5abf9cfc38d93793feeb9a97b265f92e2a70523
hayazaki <toru.hayazaki@gmail.com>
2020-12-22 10:27:53 +0900

    CodableでenumのvalueにSnakeCaseを使ってみる

--- CodableSample.playground/Sources/Person.swift
+++ CodableSample.playground/Sources/Person.swift
@@ -1,8 +1,18 @@
 import Foundation
 
 public struct Person: Codable {
+    
+    // enumのvalueは
+    // decoder.keyDecodingStrategy = .convertFromSnakeCase
+    // を指定してもSnake->Camel変換してくれないので、自分で設定する必要がある
+    enum MemberType: String, Codable {
+        case member
+        case notMember = "not_member"
+    }
+    
     var name: String?
     var age: Int?
-    var memberType: String?
+    var memberType: MemberType?
     var phoneNumber: String?
+    
 }
